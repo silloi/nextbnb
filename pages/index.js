132084198aaae4d427e1c2681fe86fa0361963e8
西山 雄大 <nishiyamayudai@team-lab.com>
2021-03-27 22:34:07 +0900

    feat: display properties

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,41 @@ export default function Home({ properties }) {
 
   return (
     <div>
-      
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
+
+      <div className="container mx-auto">
+        <div className="flex">
+          <div className="row w-full text-center my-4">
+            <h1 className="text-4xl font-bold mb-5">NextBnB</h1>
+          </div>
+        </div>
+        <div className="flex flex-row flex-wrap">
+          {properties && properties.map(property => (
+            <div className="flex-auto w-1/4 rounded overflow-hidden shadow-lg m-2">
+              <img className="w-full" src={property.image} />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{property.name} (Up to {property.guests} guests)</div>
+                <p>{property.address.street}</p>
+                <p className="text-gray-700 text-base">
+                  {property.summary}
+                </p>
+              </div>
+
+              <div className="text-center py-2 my-2 font-bold">
+                <span className="text-green-500">${property.price}</span>/night
+              </div>
+
+              <div className="text-center py-2 my-2">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-5 rounded">Book</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
@@ -19,9 +53,20 @@ export async function getServerSideProps(context) {
 
   const properties = JSON.parse(JSON.stringify(data));
 
-  console.log(properties);
+  const filtered = properties.map(property => {
+    const price = JSON.parse(JSON.stringify(property.price))
+    return { // props
+      _id: property._id,
+      name: property.name,
+      image: property.images.picture_url,
+      address: property.address,
+      summary: property.summary,
+      guests: property.accommodates,
+      price: price.$numberDecimal,
+    }
+  })
 
   return {
-    props: { properties: properties },
+    props: { properties: filtered },
   }
 }
